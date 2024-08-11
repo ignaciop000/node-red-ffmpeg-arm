c4072ab80b9e1f92657362de0b1d58374f3ad31b
Nacho <ignaciop000@gmail.com>
2024-08-11 17:05:26 -0300

    varios

--- .docker/scripts/install_devtools.sh
+++ .docker/scripts/install_devtools.sh
@@ -1,1 +1,10 @@
-../../docker-custom/scripts/install_devtools.sh
+#!/bin/bash
+set -ex
+
+# Installing Devtools
+if [[ ${TAG_SUFFIX} != *"minimal" ]]; then
+  echo "Installing devtools"
+  apk add --no-cache --virtual devtools build-base linux-headers udev python3
+else
+  echo "Skip installing devtools"
+fi
--- .docker/scripts/remove_native_gpio.sh
+++ .docker/scripts/remove_native_gpio.sh
@@ -1,1 +1,10 @@
-../../docker-custom/scripts/remove_native_gpio.sh
+#!/bin/bash
+set -ex
+
+# Remove native GPIO node if exists
+if [[ -d "/usr/src/node-red/node_modules/@node-red/nodes/core/hardware" ]]; then
+  echo "Removing native GPIO node"
+  rm -r /usr/src/node-red/node_modules/@node-red/nodes/core/hardware
+else
+  echo "Skip removing native GPIO node"
+fi
